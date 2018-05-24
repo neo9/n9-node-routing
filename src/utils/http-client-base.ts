75d011d32faf7208f9d0dbda4e3ff282eea022a1
Benjamin Daniel <benjamin.daniel@neo9.fr>
2018-05-24 14:29:40 +0200

    http-client: Remove query params from logs

--- src/utils/http-client-base.ts
+++ src/utils/http-client-base.ts
@@ -62,7 +62,7 @@ export class N9HttpClient {
 
 			return res.body as any;
 		} catch (e) {
-			this.logger.error(`Error on [${method} ${uri} ${queryParams || ''}]`, e);
+			this.logger.error(`Error on [${method} ${uri}]`, e);
 			// istanbul ignore else
 			if (e.error) {
 				throw new N9Error(e.error.code, e.statusCode);
